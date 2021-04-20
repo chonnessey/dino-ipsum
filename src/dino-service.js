3026e64aaf5cb6d81b209540cf38511388c5eaca
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-20 10:42:07 -0700

    add api call to service file
    Co-authored-by: Tori West <victoria_west@ymail.com>
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- src/dino-service.js
+++ src/dino-service.js
@@ -1,1 +1,15 @@
 // Alaskacephale
+
+let promise = new Promise(function(resolve, reject) {
+  let request = new XMLHttpRequest();
+  const url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`;
+  request.onload = function () {
+    if (this.status === 200) {
+      resolve(request.response);
+    } else {
+      reject(request.response);
+    }
+  }
+  request.open("GET", url, true);
+  request.send();
+})
